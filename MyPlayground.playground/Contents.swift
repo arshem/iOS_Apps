0595b94444aa0bbdb3b446ac62daa65057496632
Brandon Wright <bwright@arshem.com>
2017-11-29 21:34:58 -0500

    Dice Roller
    Random Dice Roller app

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+
+var myAge : Int = 32
+
+myAge = myAge+1
+
+let myName : String = "Brandon Wright"
+
+let myDetails = "\(myName) \(myAge) Male"
+
